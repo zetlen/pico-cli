e1f42e2d482ccd9f93aac9821c13540d3334d50f
jzetlen <jzetlen@adobe.com>
2023-04-06 13:58:44 -0500

    change test structure

--- tests/parse-args.js
+++ tests/parse-args.js
@@ -1,5 +1,5 @@
 import tap from "tap";
-import { parseArgs } from "./index.js";
+import { parseArgs } from "../index.js";
 
 tap.test("matching arg", (t) => {
   t.matchSnapshot(parseArgs(["--no-duh"]));
